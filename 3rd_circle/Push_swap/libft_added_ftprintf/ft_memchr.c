6b199d95d94eb3be73a733b3fa7a66782cb451e4
reprise39 <reprise39@yahoo.co.jp>
2024-12-03 23:28:14 +0900

    ssssss

---
+++ 3rd_circle/Push_swap/libft_added_ftprintf/ft_memchr.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkuida <reprise39@yahoo.co.jp>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/12 01:08:12 by mkuida            #+#    #+#             */
+/*   Updated: 2024/11/14 20:02:55 by mkuida           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	unsigned char	*dest;
+	unsigned char	target;
+	size_t			i;
+
+	if (s == NULL)
+		return (NULL);
+	dest = (unsigned char *)s;
+	target = (unsigned char)c;
+	i = 0;
+	while (n > i)
+	{
+		if (dest[i] == target)
+			return ((void *)(dest + i));
+		i++;
+	}
+	return (NULL);
+}
