20392e5ac1b79c527f3c88e4134b1603ac719334
reprise39 <reprise39@yahoo.co.jp>
2025-07-04 09:07:46 +0900

    philo add struct

--- 4th_circle/Philosophers/includes/philosophers.h
+++ 4th_circle/Philosophers/includes/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: mkuida <reprise39@yahoo.co.jp>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/26 15:50:31 by mkuida            #+#    #+#             */
-/*   Updated: 2025/07/03 18:43:50 by mkuida           ###   ########.fr       */
+/*   Updated: 2025/07/04 09:07:31 by mkuida           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,21 +39,20 @@
 # define ERROR_MAP_INWALL 4
 # define ERROR_MAP_ELEMENTS 5
 # define ERROR_MAP_CANT_PLAY 6
-
 # define ERROR_TEXTURES_EXISTS 10
 # define ERROR_TEXTURES_UNREADABLE 11
-
 # define MLX_ERROR 50
-
 # define MAP_PATH "./maps/map.ber"
-
-# define TEXTURES_PLAYER_PATH "./textures/food_merchant_spritesheet.xpm"
-# define TEXTURES_ASSET_PATH "./textures/market_assets.xpm"
-# define TEXTURES_GOAL_PATH "./textures/nature_assets.xpm"
-# define TEXTURES_WALL_PATH "./textures/market_assets.xpm"
-# define TEXTURES_FLOOR_PATH "./textures/cobblestone_tiles_brown.xpm"
-
 # define TILE_SIZE 32
+
+typedef struct s_thread_manage
+{
+	int				num_of_philos;
+	pthread_t		*thread_id;
+	pthread_mutex_t	*forks_mutex;
+	int				left_forks;
+	pthread_mutex_t	left_forks_mutex;
+}					t_thread_manage;
 
 typedef struct s_condition
 {
@@ -65,13 +64,14 @@ typedef struct s_condition
 	bool			optional_eat_times_flag;
 }					t_condition;
 
-typedef enum e_game_state
+typedef struct s_philosopher
 {
-	PLAYING,
-	GAME_CLEARED
-}					t_game_state;
-
-
+	int				id;
+	int				num_of_eat_times;
+	suseconds_t		last_eat_time;
+	t_thread_manage	*manager;		// 管理構造体へのポインタ
+	t_condition		*condition;		// 条件へのポインタ
+}					t_philosopher;
 
 // check_input.c
 int					check_argc_num(int argc);
