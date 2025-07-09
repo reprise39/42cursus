8ca9b1c6a103a84be7104047a55941f095b3c498
reprise39 <reprise39@yahoo.co.jp>
2025-07-09 14:08:22 +0900

    ex00

---
+++ 5th_circle/CPP/CPP_00/ex00/megaphone.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkuida <reprise39@yahoo.co.jp>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/08 18:30:12 by mkuida            #+#    #+#             */
+/*   Updated: 2025/07/08 18:30:12 by mkuida           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(int argc,char *argv[])
+{
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+		return (0);
+	}
+	for (int i = 1 ; i < argc ; i++)
+	{
+		for (int j = 0 ; argv[i][j] != '\0' ; j++)
+			std::cout << (char)toupper(argv[i][j]);
+	}
+	std::cout << std::endl;
+	return (0);
+}
