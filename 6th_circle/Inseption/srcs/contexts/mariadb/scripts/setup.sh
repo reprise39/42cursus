44867c520b7103c3085d641fac269e17099f642d
reprise39 <reprise39@yahoo.co.jp>
2026-02-14 02:30:27 +0900

    a

---
+++ 6th_circle/Inseption/srcs/contexts/mariadb/scripts/setup.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+#pre-boot
+mysql --skip-networking &
+
+#wait-preboot
+until mariadb-admin ping > /dev/null 2>&1; do
+  sleep 5
+done
+
+mariadb << EOF
+CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;
+CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';
+GRANT ALL PRIVILEGES ON \'${SQL_DATABASE}\`.* TO '${SQL_USER}'@'%';
+
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';
+FLUSH PRIVILEGES;
+EOF
+
+mariadb-admin -u root -p${SQL_ROOT_PASSWORD} shutdown
+
+#main bool
+exec mysqld
