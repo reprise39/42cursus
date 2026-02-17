b94ec25cfb6d9719d5ff8c53a2816641141c415c
reprise39 <reprise39@yahoo.co.jp>
2026-02-17 23:10:59 +0900

    aaa

--- 6th_circle/Inseption/srcs/contexts/wordpress/scripts/setup.sh
+++ 6th_circle/Inseption/srcs/contexts/wordpress/scripts/setup.sh
@@ -1,2 +1,35 @@
 #!/bin/bash
 
+#wait db
+until mariadb-admin ping -h"mariadb" --silent; do
+    sleep 1
+done
+
+
+if ! wp core is-installed --allow-root; then
+
+    wp core download --allow-root
+
+    wp config create --allow-root \
+        --dbname=${SQL_DATABASE} \
+        --dbuser=${SQL_USER} \
+        --dbpass=${SQL_PASSWORD} \
+        --dbhost=mariadb:3306
+
+    # 4. サイトのインストール（管理者ユーザーの作成）
+    wp core install --allow-root \
+        --url=${DOMAIN_NAME} \
+        --title=${SITE_TITLE} \
+        --admin_user=${ADMIN_USER} \
+        --admin_password=${ADMIN_PASSWORD} \
+        --admin_email=${ADMIN_EMAIL}
+
+    # 5. 一般ユーザーの作成（課題要件）
+    wp user create ${USER_NAME} ${USER_EMAIL} --user_pass=${USER_PASSWORD} --role=author --allow-root
+
+fi
+
+# 6. PHP-FPMをフォアグラウンドで起動
+# -F はデーモン化（バックグラウンド化）させないためのフラグ
+mkdir -p /run/php
+exec php-fpm7.4 -F
